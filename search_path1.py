57865d008703c757d4c53f05cf3a13aa5d384f82
sks731953873 <731953873@qq.com>
2016-08-03 03:52:38 +0800

    search_path1.py

---
+++ search_path1.py
@@ -0,0 +1,26 @@
+#输入搜索路径，查找改路径下所有的（包括子文件）下所有视屏格式（rmv，MP4都可以），并创建一个文件将所有文件路径存放到该文件。
+
+import os
+
+def search_file(start_dir, target) :
+    os.chdir(start_dir)
+    
+    for each_file in os.listdir(os.curdir) :
+        ext = os.path.splitext(each_file)[1]
+        if ext in target :
+            vedio_list.append(os.getcwd() + os.sep + each_file + os.linesep) # 使用os.sep是程序更标准
+        if os.path.isdir(each_file) :
+            search_file(each_file, target) # 递归调用
+            os.chdir(os.pardir) # 递归调用后切记返回上一层目录
+
+start_dir = input('请输入待查找的初始目录：')
+program_dir = os.getcwd()
+
+target = ['.mp4', '.avi', '.rmvb']
+vedio_list = []
+
+search_file(start_dir, target)
+
+f = open(program_dir + os.sep + 'vedioList.txt', 'w')
+f.writelines(vedio_list)
+f.close()
